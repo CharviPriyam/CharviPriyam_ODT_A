f7edf8d2676dba9f5cf45dba234a8948ae523b0a
CharviPriyam <charvi.priyam2025@bitsdesign.edu.in>
2026-02-17 01:41:04 +0530

    Create Stepper Mode - Half Step Mode (Shorter).py

---
+++ Codes/Week 7/Stepper Mode - Half Step Mode (Shorter).py
@@ -0,0 +1,19 @@
+#servo motor
+from machine import Pin
+import time
+
+l1=Pin(5,Pin.OUT)
+l2=Pin(14,Pin.OUT)
+l3=Pin(18,Pin.OUT)
+l4=Pin(19,Pin.OUT)
+
+l=[[1,0,0,0,0,0,1,1],[1,1,1,0,0,0,0,0],[0,0,1,1,1,0,0,0],[0,0,0,0,1,1,1,0]]
+while True:
+    for i in l:
+        l1.value(i[0])
+        l2.value(i[1])
+        l3.value(i[2])
+        l4.value(i[3])
+        time.sleep_ms(5) #5 milli seconds
+        
+#shorter code 
