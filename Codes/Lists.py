ecf834ec5e38fd7d10b8b0c6692f4f21403f594e
CharviPriyam <charvi.priyam2025@bitsdesign.edu.in>
2026-02-16 18:50:56 +0530

    Create Lists.py

---
+++ Codes/Lists.py
@@ -0,0 +1,12 @@
+l=["Charvi","Ava","Siddhi","Khushi","Vasu"]
+print(l)
+l.append("boo")
+print(l)
+l.extend(["hoo","loo"])
+print(l)
+l.pop(2)
+print(l)
+for i in l:
+    print(i)
+for i in range(1,6):
+    print(l[i])
