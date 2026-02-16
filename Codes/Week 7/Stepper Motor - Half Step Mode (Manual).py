8e23e43f9b88d5bccb6bd3153cfb56b7712ca795
CharviPriyam <charvi.priyam2025@bitsdesign.edu.in>
2026-02-17 01:39:23 +0530

    Create Stepper Motor - Half Step Mode (Manual).py

---
+++ Codes/Week 7/Stepper Motor - Half Step Mode (Manual).py
@@ -0,0 +1,50 @@
+#servo motor
+
+from machine import Pin
+import time
+l1=Pin(5,Pin.OUT)
+l2=Pin(14,Pin.OUT)
+l3=Pin(18,Pin.OUT)
+l4=Pin(19,Pin.OUT)
+
+while True:
+    #1st Second
+    l1.value(1)      #ms = milli second
+    l2.value(0)      #us = micro second
+    l3.value(0)      #half step
+    l4.value(0)
+    l1.value(0)
+    l2.value(0)
+    l3.value(1)
+    l4.value(1)
+    time.sleep_ms(5)
+    #2nd Second
+    l1.value(1)
+    l2.value(1)
+    l3.value(1)
+    l4.value(0)
+    l1.value(0)
+    l2.value(0)
+    l3.value(0)
+    l4.value(0)
+    time.sleep_ms(5)
+    #3rd Second
+    l1.value(0)
+    l2.value(0)
+    l3.value(1)
+    l4.value(1)
+    l1.value(1)
+    l2.value(0)
+    l3.value(0)
+    l4.value(0)
+    time.sleep_ms(5)
+    #4th Second
+    l1.value(0)
+    l2.value(0)
+    l3.value(0)
+    l4.value(0)
+    l1.value(1)
+    l2.value(1)
+    l3.value(1)
+    l4.value(0)
+    time.sleep_ms(5)
