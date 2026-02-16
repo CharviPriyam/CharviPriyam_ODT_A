e4b0d249cabc06e541f13586ceae8ea286cce61b
CharviPriyam <charvi.priyam2025@bitsdesign.edu.in>
2026-02-16 19:19:24 +0530

    Create IF, ELIF, ELSE explorations.py

---
+++ Codes/IF, ELIF, ELSE explorations.py
@@ -0,0 +1,28 @@
+x=5
+#case 1
+if x>0:
+    print("ok")
+elif x==5:
+    print("okk")
+elif x<10:
+    print("okkk")
+
+#case 2
+if x>0:
+    print("ok")
+if x==5:
+    print("okk")
+if x<0:
+    print("okkk")
+else:
+    print("no")
+
+#case 3
+if x<0:
+    print("ok")
+elif x==5:
+    print("okk")   
+elif x>0:
+    print("okkk")
+else:
+    print("no")
