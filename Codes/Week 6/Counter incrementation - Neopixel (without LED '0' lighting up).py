fdb906f3296c116d70a02abd1125c37bc7c11203
CharviPriyam <charvi.priyam2025@bitsdesign.edu.in>
2026-02-16 22:57:41 +0530

    Create Counter incrementation - Neopixel (without LED '0' lighting up).py

---
+++ Codes/Week 6/Counter incrementation - Neopixel (without LED '0' lighting up).py
@@ -0,0 +1,20 @@
+#LED no. 0 did not light up coz of increment
+from machine import Pin
+import time
+import neopixel
+
+n=neopixel.NeoPixel(Pin(12),16)
+s=Pin(4,Pin.IN,Pin.PULL_UP)
+i=0
+
+while True:
+  s_val=s.value()
+  if s_val==0:
+    i+=1
+    time.sleep(0.5)
+    print(i)
+    n[i]=(255,0,0)
+    time.sleep(0.1)
+    n.write()
+  
+    
