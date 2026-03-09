f9900cdbc2b973be408a5797e5be6b602bbf8f45
CharviPriyam <charvi.priyam2025@bitsdesign.edu.in>
2026-03-09 19:02:30 +0530

    Create Reaction timer [Alt method with pass].py

---
+++ Codes/Week 9/Reaction timer [Alt method with pass].py
@@ -0,0 +1,20 @@
+from machine import Pin
+import time
+import random
+
+l=Pin(12,Pin.OUT)
+pb=Pin(4,Pin.IN,Pin.PULL_UP)
+
+while True:
+    r=random.randrange(0,6)
+    l.off()
+    time.sleep(r)
+    l.on()
+    x=time.ticks_ms()
+    while pb.value()==1:
+        pass
+    y=time.ticks_ms()
+    l.off()
+    t=time.ticks_diff(y,x)
+    print(t)
+    time.sleep(0.1)
